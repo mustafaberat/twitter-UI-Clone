ecbce4c9935159f7c97336a7486f63e0648b4048
Mustafa Berat <32976555+mustafaberat@users.noreply.github.com>
2020-06-29 23:50:05 +0300

    Configure Storybook with Next.js, CSS Modules

--- stories/1-Button.stories.js
+++ stories/1-Button.stories.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import { action } from '@storybook/addon-actions';
-import { Button } from '@storybook/react/demo';
+
+import Button from '../components/button'
 
 export default {
   title: 'Button',
   component: Button,
 };
 
-export const Text = () => <Button onClick={action('clicked')}>Hello Button</Button>;
-
 export const Emoji = () => (
-  <Button onClick={action('clicked')}>
+  <Button>
     <span role="img" aria-label="so cool">
       😀 😎 👍 💯
     </span>
