7e8f6ba03e8d55b2eaf9fea899b558a680c97082
Mustafa Berat <32976555+mustafaberat@users.noreply.github.com>
2020-07-01 21:02:41 +0300

    allIcon story is done

---
+++ stories/1-Icons.stories.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import * as AllIcons from '../components/icons'
+
+export default {
+  title: 'Icons',
+};
+
+export const allIcons = () => (
+  <div className="allicons">
+    <AllIcons.ArrowBottom />
+    <AllIcons.Bookmark />
+    <AllIcons.BookmarkFill />
+    <AllIcons.Emoji />
+    <AllIcons.Explore />
+    <AllIcons.ExplorerFill />
+    <AllIcons.Gif />
+    <AllIcons.Home />
+    <AllIcons.HomeFill />
+    <AllIcons.Like />
+    <AllIcons.Lists />
+    <AllIcons.ListsFill />
+    <AllIcons.Media />
+    <AllIcons.Messages />
+    <AllIcons.MessagesFill />
+    <AllIcons.More />
+    <AllIcons.Notification />
+    <AllIcons.NotificationFill />
+    <AllIcons.Options />
+    <AllIcons.Profile />
+    <AllIcons.ProfileFill />
+    <AllIcons.Question />
+    <AllIcons.Reply />
+    <AllIcons.Retweet />
+    <AllIcons.Search />
+    <AllIcons.Share />
+    <AllIcons.TimelineProp />
+    <AllIcons.Twitter />
+  </div>
+);
