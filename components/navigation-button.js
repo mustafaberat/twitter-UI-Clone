4e0810912cef2603a157b9f5c85b497ca61e02f4
Mustafa Berat <32976555+mustafaberat@users.noreply.github.com>
2020-07-02 13:37:02 +0300

    selectedItem is done

--- components/navigation-button.js
+++ components/navigation-button.js
@@ -1,12 +1,14 @@
 // BASE BUTTON
 import React from 'react';
 
+import classNameMerger from 'classnames'
+
 import Button from './button'
 import styles from './navigation-button.module.css';
 
-function NavButton({ children }) {
+function NavButton({ selected, children, ...props }) {
     return (
-        <Button className={styles.navButton}>
+        <Button className={classNameMerger(styles.navButton, selected && styles.navButtonSelected)} {...props}>
             {children}
         </Button>)
 }
--- components/navigation.js
+++ components/navigation.js
@@ -6,41 +6,41 @@ import TitleBold from '../components/title-bold';
 
 import styles from './navigation.module.css'
 
-function Navigation() {
+function Navigation({ selectedKey }) {
     return (
         <nav className={styles.nav}>
             <NavButton>
                 <Twitter />
             </NavButton>
-            <NavButton>
+            <NavButton selected={selectedKey === "home"}>
                 <Home />
                 <TitleBold>Home</TitleBold>
             </NavButton>
-            <NavButton>
+            <NavButton selected={selectedKey === "explore"}>
                 <Explore />
                 <TitleBold>Explore</TitleBold>
             </NavButton>
-            <NavButton>
+            <NavButton selected={selectedKey === "notification"}>
                 <Notification />
                 <TitleBold>Notification</TitleBold>
             </NavButton>
-            <NavButton>
+            <NavButton selected={selectedKey === "messages"}>
                 <Messages />
                 <TitleBold>Messages</TitleBold>
             </NavButton>
-            <NavButton>
+            <NavButton selected={selectedKey === "lists"}>
                 <Lists />
                 <TitleBold>Lists</TitleBold>
             </NavButton>
-            <NavButton>
+            <NavButton selected={selectedKey === "profile"}>
                 <Profile />
                 <TitleBold>Profile</TitleBold>
             </NavButton>
-            <NavButton>
+            <NavButton selected={selectedKey === "more"}>
                 <More />
                 <TitleBold>More</TitleBold>
             </NavButton>
-        </nav>
+        </nav >
     )
 }
 export default Navigation
