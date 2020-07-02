c525327d6bab40185ad2ca8e24239348a32e69bb
Mustafa Berat <32976555+mustafaberat@users.noreply.github.com>
2020-07-02 13:01:08 +0300

    adding classnames to merge classnames and usage

--- components/button.js
+++ components/button.js
@@ -1,9 +1,11 @@
 // BASE BUTTON
 import React from 'react';
 
+import classNamesMerger from 'classnames'
+
 import styles from './button.module.css';
 
-function Button({ children, ...props }) {
-    return <button className={styles.button} {...props} type="button">{children}</button>
+function Button({ children, className, ...props }) {
+    return <button className={classNamesMerger(styles.button, className)} {...props} type="button">{children}</button>
 }
 export default Button
