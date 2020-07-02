d74d10859eb7cf4e248aea8f30bfed678f9bd8e6
Mustafa Berat <32976555+mustafaberat@users.noreply.github.com>
2020-07-02 13:37:15 +0300

    sample buttons added into story

--- stories/2-Buttons.stories.js
+++ stories/2-Buttons.stories.js
@@ -4,10 +4,32 @@ import Button from "../components/button";
 import NavigationButton from "../components/navigation-button";
 import Navigation from "../components/navigation";
 
+import { Home } from '../components/icons/index';
+import TitleBold from '../components/title-bold';
+
+
 export default {
     title: 'Buttons',
 };
 
 export const BasicButton = () => (<Button>Basic Button</Button>);
-export const NavButton = () => (<NavigationButton>Nav Button</NavigationButton>);
-export const Nav = () => (<Navigation />);
+
+export const NavButtons = () => (
+    <div>
+        <NavigationButton>
+            <Home />
+            <TitleBold>Sample Home Button</TitleBold>
+        </NavigationButton>
+        <NavigationButton>
+            <TitleBold>Only Text Button</TitleBold>
+        </NavigationButton>
+        <div style={{ display: "flex", alignItems: "center" }}>
+            <h2 style={{ color: "white", marginRight: 20 }}>Only Icon: </h2>
+            <NavigationButton>
+                <Home />
+            </NavigationButton>
+        </div>
+    </div>
+);
+
+export const NavigationTotal = () => (<Navigation selectedKey="home" />);
